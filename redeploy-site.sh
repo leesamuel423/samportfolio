4dd77efbabd2beea6ebaba88c9b24c6fa62fa5c8
Samuel Lee <leesamuel423@gmail.com>
2024-07-14 20:43:06 -0400

    update redeploy-site script for systemd redeployment

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 # Author: leesamuel423
 
-# Step 1: Kill all existing tmux sessions
-tmux kill-server
 
-# Step 2: Change to project directory
+# Step 1: Change to project directory
 cd samportfolio || exit
 
-# Step 3: Fetch and reset the git repository to the latest changes from the main branch
+# Step 2: Fetch and reset the git repository to the latest changes from the main branch
 git fetch && git reset origin/main --hard
 
-# Step 4: Enter the python virtual environment and install python dependencies
+# Step 3: Enter the python virtual environment and install python dependencies
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-# Step 5: Start a new detached tmux session that starts the Flask server
-tmux new -s portfolio "source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+# Step 4: Restart myportfolio service
+sudo systemctl restart myportfolio
+
+# Step 5: Check status of service
+sudo systemctl status myportfolio

aea7095ca4f86580cf99e39ac45037847a42e2d2
Samuel Lee <leesamuel423@gmail.com>
2024-07-26 00:38:52 -0400

    update redeployment script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -8,12 +8,11 @@ cd samportfolio || exit
 # Step 2: Fetch and reset the git repository to the latest changes from the main branch
 git fetch && git reset origin/main --hard
 
-# Step 3: Enter the python virtual environment and install python dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+# Step 3: Spin down existing containers
+docker compose -f docker-compose.prod.yml down
 
-# Step 4: Restart myportfolio service
-sudo systemctl restart myportfolio
+# Step 4: Build and start containers
+docker compose -f docker-compose.prod.yml up -d --build
 
-# Step 5: Check status of service
-sudo systemctl status myportfolio
+# Step 5: Check status of containers
+docker ps
